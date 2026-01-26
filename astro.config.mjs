e702f2ac4a8eac473d31fe355bc1b8d624f12fe9
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2026-01-26 17:29:56 -0500

    build(Astro): upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: 'https://necrogoru.github.io',
-  base: '/cv'
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 });
