3ce2534959988e4656653c4a8a93906d0e0bf1fb
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2026-09-04 16:04:33 -0500

    feat: restore language usinng the client side instead of the ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/cv',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
   integrations: [],
   vite: {
     plugins: [
