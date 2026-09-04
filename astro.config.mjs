6847e7554fa2eaebc61b77c39515cbf77381e9ad
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2026-09-04 16:32:41 -0500

    chore: upgrade packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   base: '/cv',
+  compressHTML: true,
   integrations: [],
   vite: {
     plugins: [
